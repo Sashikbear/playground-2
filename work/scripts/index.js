f111ae088e4267537be713b7f0e11d5369211471
benyossef27 <93250111+benyossef27@users.noreply.github.com>
2022-01-21 13:06:42 +0200

    popup form + open + close on esc & clickout

---
+++ work/scripts/index.js
@@ -0,0 +1,38 @@
+                        //veriables
+const popupOpenButton = document.querySelector("#resume")
+const popupwork = document.querySelector(".popup_type_work");
+
+
+                        //functions
+//open generic popup
+function OpenPopup(popup){
+    popup.classList.add("popup_opened")
+    popup.addEventListener("click", handleOverlayClick)
+    document.addEventListener("keydown", handleClosePopupOnEsc)
+}
+
+//close popup
+function closePopup(popup){
+    popup.classList.remove("popup_opened")
+    popup.removeEventListener("click", handleOverlayClick)
+    document.removeEventListener("keydown", handleClosePopupOnEsc)
+}
+
+//close popup when clicking outside
+function handleOverlayClick(event) {
+    closePopup(event.target);
+}
+//close pop up on Esc
+function handleClosePopupOnEsc(event){
+    const popup = document.querySelector(".popup_opened");
+    if(event.key === "Escape"){
+        closePopup(popup);
+    }
+}
+
+                        //event listeners
+// open resume popup
+popupOpenButton.addEventListener("click", () => OpenPopup(popupwork)
+);
+
+
